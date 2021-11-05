56c85537f7a674b7511477672b76619c90342c0e
James Jawanda <james.jawanda@gmail.com>
2021-11-05 13:06:58 -0400

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function compute()
 {
 
-    var principal = document.getElementById("principal").value;
+     p = document.getElementById("principal").value;
 
-    if(principal =  principal <= 0)
+    if(  p <= 0)
     {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
@@ -18,16 +18,14 @@ function compute()
      yearNow = parseInt(dateNow.getFullYear()) + parseInt(n);
     
     resultDisplay = document.getElementById("result");
-    resultDisplay.innerHTML = "If you deposit " + "$<span class='highlight'>" + p + "</span>"  + ", <br> at an interest rate of " + "<span class='highlight'>" + r + "%</span>." + "<br> You will receive an amount of " + "$<span class='highlight'>" + i + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+    resultDisplay.innerHTML = "If you deposit " + "$<span class='highlight'>" + p + "</span>"  + ", <br> at an interest rate of " + "<span class='highlight'>" + r + "</span>" + "%" + "<br> You will receive an amount of " + "$<span class='highlight'>" + i + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>" + ".";
 }
-
-
 
 function SliderValue()
 {
 
-var slider = document.getElementById("rate");
-var output = document.getElementById("rate_display");
+slider = document.getElementById("rate");
+output = document.getElementById("rate_display");
 output.innerHTML = slider.value;
 slider.oninput = function() {
 output.innerHTML = this.value;

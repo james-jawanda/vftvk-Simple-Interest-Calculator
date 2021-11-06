77ad4db2b68956297d78bd21179e29e257bb4c3f
James Jawanda <james.jawanda@gmail.com>
2021-11-06 12:13:28 -0400

    Final Copy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function compute()
 {
 
-     p = document.getElementById("principal").value;
+ var p = document.getElementById("principal").value;
 
     if(  p <= 0)
     {
@@ -9,23 +9,23 @@ function compute()
         document.getElementById("principal").focus();
         return;
     }
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    n = document.getElementById("years").value;
-    i = (p*r*n)/100;
+ var  p = document.getElementById("principal").value;
+ var   r = document.getElementById("rate").value;
+ var   n = document.getElementById("years").value;
+ var  i = (p*r*n)/100;
 
-     dateNow = new Date();
-     yearNow = parseInt(dateNow.getFullYear()) + parseInt(n);
+ var   dateNow = new Date();
+ var   yearNow = parseInt(dateNow.getFullYear()) + parseInt(n);
     
-    resultDisplay = document.getElementById("result");
+ var   resultDisplay = document.getElementById("result");
     resultDisplay.innerHTML = "If you deposit " + "$<span class='highlight'>" + p + "</span>"  + ", <br> at an interest rate of " + "<span class='highlight'>" + r + "</span>" + "%" + "<br> You will receive an amount of " + "$<span class='highlight'>" + i + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>" + ".";
 }
 
 function SliderValue()
 {
 
-slider = document.getElementById("rate");
-output = document.getElementById("rate_display");
+var    slider = document.getElementById("rate");
+var    output = document.getElementById("rate_display");
 output.innerHTML = slider.value;
 slider.oninput = function() {
 output.innerHTML = this.value;
